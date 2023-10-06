2b833adf663444879a3780df9fb29642bbc2f27c
Anna_nee_zm <112720662+MaksimRedko@users.noreply.github.com>
2023-10-06 17:17:05 +0300

    Лабораторная работа 1

---
+++ lab_1/разделение игроков на две команды.py
@@ -0,0 +1,8 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+# Определяем первую и вторую команды
+first = list_players[0:len(list_players) // 2]
+second = list_players[len(list_players) // 2:]
+
+print(first)
+print(second)
