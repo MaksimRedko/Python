198246551ecad9a505e22686b5464cfdcd04aae9
Anna_nee_zm <112720662+MaksimRedko@users.noreply.github.com>
2023-10-06 17:15:59 +0300

    Delete lab_1 directory

--- lab_1/замена пропущенного элемента.py
+++
@@ -1,7 +0,0 @@
-numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-
-index_None_element = numbers.index(None)
-sum_ = sum(numbers[0:index_None_element]) + sum(numbers[index_None_element + 1:])
-len_ = len(numbers)
-numbers[index_None_element] = sum_ / len_
-print("Измененный список:", numbers)
--- lab_1/посещение сайта.py
+++
@@ -1,5 +0,0 @@
-users = ['user1', 'user2', 'user3', 'user1', 'user4', 'user2']
-dict_users = {"Общее количество": 0, "Уникальные посещения": 0}
-dict_users["Общее количество"] = len(users)
-dict_users["Уникальные посещения"] = len(set(users))
-print(dict_users)
